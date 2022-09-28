52312179b6e3a84dbd69295cf593afffde51953f
Mohsin Yaqoob <mohsinyqb@gmail.com>
2022-09-28 22:06:04 +0530

    chore: add email info to hire me page

--- src/pages/hire-me/index.tsx
+++ src/pages/hire-me/index.tsx
@@ -1,5 +1,23 @@
+import { Link, Text, VStack } from "@chakra-ui/react";
+import { NextSeo } from "next-seo";
+import React from "react";
+
 const HireMe = () => {
-  return <div>Hire Me!</div>;
+  return (
+    <React.Fragment>
+      {" "}
+      <NextSeo title="Fitness - Mohsin Yaqoob" />
+      <VStack mt={[8, 16, 16]} justifyContent={"start"} align={"left"}>
+        <Text>
+          Please write me an email at{" "}
+          <Link color={"accent"} href="mailto:hi@mohsinyaqoob.com">
+            {" "}
+            hi@mohsinyaqoob.com
+          </Link>
+        </Text>
+      </VStack>
+    </React.Fragment>
+  );
 };
 
 export default HireMe;

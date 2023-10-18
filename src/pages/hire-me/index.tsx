bb52273028c42b3e300a271f8eceb98e746f119b
Mohsin Yaqoob <45480655+mohsinyaqoob@users.noreply.github.com>
2023-10-18 21:34:59 +0530

    chore: update hire me page

--- src/pages/hire-me/index.tsx
+++ src/pages/hire-me/index.tsx
@@ -20,20 +20,39 @@ const HireMe = () => {
           Hir me (Before my mom does)
         </Text>
 
+        <Heading as='h2' size='xl'>
+          I Never Met a Deadline I Didn't Like:
+        </Heading>
         <Text>
-          I Never Met a Deadline I Didn't Like: I've been known to chase deadlines like they're the last piece of pizza at a party – with unrelenting determination and a touch of desperation.
+           I've been known to chase deadlines like they're the last piece of pizza at a party – with unrelenting determination and a touch of desperation.
         </Text>
+
+        <Heading as='h2' size='xl'>
+          My Coffee Addiction is Your Gain: 
+        </Heading>
         <Text>
-          My Coffee Addiction is Your Gain: With a constant caffeine buzz, you can expect me to be as alert as a meerkat on espresso, ready to tackle any task with hyper-caffeinated enthusiasm.
+          With a constant caffeine buzz, you can expect me to be as alert as a meerkat on espresso, ready to tackle any task with hyper-caffeinated enthusiasm.
         </Text>
+
+        <Heading as='h2' size='xl'>
+         I Speak Fluent Emoji:
+        </Heading>
         <Text>
-          I Speak Fluent Emoji: I've got emojis for every situation. Need an urgent response? 💡 On vacation but still working? 🏖️ Stuck in a never-ending Zoom call? 😵 I've got you covered.
+           I've got emojis for every situation. Need an urgent response? 💡 On vacation but still working? 🏖️ Stuck in a never-ending Zoom call? 😵 I've got you covered.
         </Text>
+
+        <Heading as='h2' size='xl'>
+         I'm a Multitasking Maestro:
+        </Heading>
         <Text>
           I'm a Multitasking Maestro: Like a plate-spinner at a circus, I can juggle multiple tasks without dropping a single one – well, most of the time.
         </Text>
+
+        <Heading as='h2' size='xl'>
+         A Team Player with an MVP Spirit:
+        </Heading>
         <Text>
-          A Team Player with an MVP Spirit: I'm not just a team player; I'm the cheerleader, coach, and water boy all rolled into one. I'm the kind of colleague who brings donuts on Mondays just to make your week a little brighter.
+           I'm not just a team player; I'm the cheerleader, coach, and water boy all rolled into one. I'm the kind of colleague who brings donuts on Mondays just to make your week a little brighter.
         </Text>       
       </VStack>
     </React.Fragment>
